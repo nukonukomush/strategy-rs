01a4b7e02c2281c606a7d4becfc9bdc8aedc58aa
nukonukomush <you@example.com>
2019-08-27 18:35:21 +0900

    update

--- src/indicator.rs
+++ src/indicator.rs
@@ -4,50 +4,96 @@ use std::cell::RefCell;
 use std::os::raw::*;
 use std::rc::Rc;
 
+pub enum MaybeValue<V> {
+    Value(V),
+    OutOfRange,
+}
+
+// Indiator は、かならず Granularity をもつ。
+// Granularity の変換の際、一番小さい Granularity (1秒) にもどして相互変換する。
+
 pub trait Indicator<G, V> {
-    fn value(&self, time: Time<G>) -> Option<V>;
     fn granularity(&self) -> G;
 }
 
-impl<G, V> Indicator<G, V> for &dyn Indicator<G, V> {
-    #[allow(unconditional_recursion)]
-    fn value(&self, time: Time<G>) -> Option<V> {
-        self.value(time)
-    }
-
-    #[allow(unconditional_recursion)]
-    fn granularity(&self) -> G {
-        self.granularity()
-    }
+pub trait FuncIndicator<G, V>: Indicator<G, V> {
+    fn value(&self, time: Time<G>) -> MaybeValue<V>;
 }
+
+pub trait IterIndicator<G, V>: Indicator<G, V> {
+    // fn into_iter(self) -> IntoIterator<Item=V>;
+
+    fn next(&mut self) -> MaybeValue<V>;
+}
+
+pub trait Provisional<G, V> {
+    fn provisional_value(&self, time: Time<G>) -> MaybeValue<V>;
+}
+
+// impl<G, V> Indicator<G, V> for &dyn Indicator<G, V> {
+//     #[allow(unconditional_recursion)]
+//     fn value(&self, time: Time<G>) -> Option<V> {
+//         self.value(time)
+//     }
+
+//     #[allow(unconditional_recursion)]
+//     fn granularity(&self) -> G {
+//         self.granularity()
+//     }
+// }
 
 impl<G, V, I> Indicator<G, V> for RefCell<I>
 where
-    V: Clone,
     I: Indicator<G, V>,
 {
-    fn value(&self, time: Time<G>) -> Option<V> {
-        let inner = self.borrow();
-        (*inner).value(time)
-    }
     fn granularity(&self) -> G {
-        let inner = self.borrow();
-        (*inner).granularity()
+        (*self.borrow()).granularity()
     }
 }
 
+impl<G, V, I> FuncIndicator<G, V> for RefCell<I>
+where
+    I: FuncIndicator<G, V>,
+{
+    fn value(&self, time: Time<G>) -> MaybeValue<V> {
+        (*self.borrow()).value(time)
+    }
+}
+
+impl<G, V, I> IterIndicator<G, V> for RefCell<I>
+where
+    I: IterIndicator<G, V>,
+{
+    fn next(&mut self) -> MaybeValue<V> {
+        (*self.borrow_mut()).next()
+    }
+}
+
+use std::ops::Deref;
 impl<G, V, I> Indicator<G, V> for Rc<I>
 where
-    V: Clone,
     I: Indicator<G, V>,
 {
-    #[allow(unconditional_recursion)]
-    fn value(&self, time: Time<G>) -> Option<V> {
-        self.value(time)
-    }
-    #[allow(unconditional_recursion)]
     fn granularity(&self) -> G {
-        self.granularity()
+        self.deref().granularity()
+    }
+}
+
+impl<G, V, I> FuncIndicator<G, V> for Rc<I>
+where
+    I: FuncIndicator<G, V>,
+{
+    fn value(&self, time: Time<G>) -> MaybeValue<V> {
+        self.deref().value(time)
+    }
+}
+
+impl<G, V, I> IterIndicator<G, V> for Rc<RefCell<I>>
+where
+    I: IterIndicator<G, V>,
+{
+    fn next(&mut self) -> MaybeValue<V> {
+        (*self.borrow_mut()).next()
     }
 }
 
@@ -87,92 +133,92 @@ pub mod tests {
     // }
 }
 
-pub mod ffi {
-    use super::*;
-    use crate::time::ffi::*;
+// pub mod ffi {
+//     use super::*;
+//     use crate::time::ffi::*;
 
-    pub unsafe fn destroy<T>(ptr: *mut T) {
-        if ptr.is_null() {
-            return;
-        }
-        // ここ Box にする必要ある？？
-        let boxed = Box::from_raw(ptr);
-        drop(boxed);
-    }
+//     pub unsafe fn destroy<T>(ptr: *mut T) {
+//         if ptr.is_null() {
+//             return;
+//         }
+//         // ここ Box にする必要ある？？
+//         let boxed = Box::from_raw(ptr);
+//         drop(boxed);
+//     }
 
-    #[derive(Clone)]
-    pub struct IndicatorPtr<V>(pub Rc<RefCell<dyn Indicator<VarGranularity, V>>>);
+//     #[derive(Clone)]
+//     pub struct IndicatorPtr<V>(pub Rc<RefCell<dyn Indicator<VarGranularity, V>>>);
 
-    impl<V> Indicator<VarGranularity, V> for IndicatorPtr<V> {
-        fn value(&self, time: Time<VarGranularity>) -> Option<V> {
-            self.borrow().value(time)
-        }
-        fn granularity(&self) -> VarGranularity {
-            self.borrow().granularity()
-        }
-    }
+//     impl<V> Indicator<VarGranularity, V> for IndicatorPtr<V> {
+//         fn value(&self, time: Time<VarGranularity>) -> Option<V> {
+//             self.borrow().value(time)
+//         }
+//         fn granularity(&self) -> VarGranularity {
+//             self.borrow().granularity()
+//         }
+//     }
 
-    use std::ops::Deref;
-    impl<V> Deref for IndicatorPtr<V> {
-        type Target = Rc<RefCell<dyn Indicator<VarGranularity, V>>>;
-        fn deref(&self) -> &Self::Target {
-            &self.0
-        }
-    }
+//     use std::ops::Deref;
+//     impl<V> Deref for IndicatorPtr<V> {
+//         type Target = Rc<RefCell<dyn Indicator<VarGranularity, V>>>;
+//         fn deref(&self) -> &Self::Target {
+//             &self.0
+//         }
+//     }
 
-    macro_rules! define_value {
-        ($t:ident, $value:ident) => {
-            #[no_mangle]
-            pub unsafe extern "C" fn $value(
-                ptr: *mut IndicatorPtr<$t>,
-                time: CTime,
-            ) -> COption<$t> {
-                if ptr.is_null() {
-                    return COption::none();
-                }
+//     macro_rules! define_value {
+//         ($t:ident, $value:ident) => {
+//             #[no_mangle]
+//             pub unsafe extern "C" fn $value(
+//                 ptr: *mut IndicatorPtr<$t>,
+//                 time: CTime,
+//             ) -> COption<$t> {
+//                 if ptr.is_null() {
+//                     return COption::none();
+//                 }
 
-                let ptr = &*ptr;
-                COption::from_option(ptr.borrow().value(time.into()))
-            }
-        };
-    }
-    macro_rules! define_value_convert {
-        ($t1:ident, $t2:ident, $value:ident) => {
-            #[no_mangle]
-            pub unsafe extern "C" fn $value(
-                ptr: *mut IndicatorPtr<$t1>,
-                time: CTime,
-            ) -> COption<$t2> {
-                if ptr.is_null() {
-                    return COption::none();
-                }
+//                 let ptr = &*ptr;
+//                 COption::from_option(ptr.borrow().value(time.into()))
+//             }
+//         };
+//     }
+//     macro_rules! define_value_convert {
+//         ($t1:ident, $t2:ident, $value:ident) => {
+//             #[no_mangle]
+//             pub unsafe extern "C" fn $value(
+//                 ptr: *mut IndicatorPtr<$t1>,
+//                 time: CTime,
+//             ) -> COption<$t2> {
+//                 if ptr.is_null() {
+//                     return COption::none();
+//                 }
 
-                let ptr = &*ptr;
-                COption::from_option(ptr.borrow().value(time.into()).map($t2::from))
-            }
-        };
-    }
-    define_value!(f64, indicator_value_f64);
-    define_value!(i32, indicator_value_i32);
-    use cross::ffi::*;
-    use cross::*;
-    define_value_convert!(CrossState, CCrossState, indicator_value_cross);
-    use crate::position::*;
-    use crate::position::ffi::*;
-    define_value_convert!(SimplePosition, CSimplePosition, indicator_value_simpleposition);
-    use trailing_stop::ffi::*;
-    use trailing_stop::*;
-    define_value_convert!(TrailingStopSignal, CTrailingStopSignal, indicator_value_trailingstopsignal);
-}
+//                 let ptr = &*ptr;
+//                 COption::from_option(ptr.borrow().value(time.into()).map($t2::from))
+//             }
+//         };
+//     }
+//     define_value!(f64, indicator_value_f64);
+//     define_value!(i32, indicator_value_i32);
+//     use cross::ffi::*;
+//     use cross::*;
+//     define_value_convert!(CrossState, CCrossState, indicator_value_cross);
+//     use crate::position::*;
+//     use crate::position::ffi::*;
+//     define_value_convert!(SimplePosition, CSimplePosition, indicator_value_simpleposition);
+//     use trailing_stop::ffi::*;
+//     use trailing_stop::*;
+//     define_value_convert!(TrailingStopSignal, CTrailingStopSignal, indicator_value_trailingstopsignal);
+// }
 
-pub mod cached;
-pub mod stream;
-pub mod complement;
-pub mod convert_granularity;
-pub mod cross;
-pub mod ordering;
-pub mod sma;
-pub mod vec;
-pub mod func;
-pub mod slope;
-pub mod trailing_stop;
+// pub mod cached;
+// pub mod stream;
+// pub mod complement;
+// pub mod convert_granularity;
+// pub mod cross;
+// pub mod ordering;
+// pub mod sma;
+// pub mod vec;
+// pub mod func;
+// pub mod slope;
+// pub mod trailing_stop;
